2763a5f2f577ff0933314a1bce1d5b4daeab9a0f
Josetechcode <chema1987@hotmail.com>
2024-11-28 15:00:41 -0500

    while

--- eje1.py
+++ eje1.py
@@ -1,17 +1,26 @@
 # DISPLAYING THE PROGRAM TITLE
 print("*** PROGRAMA QUE DETERMINA EL PRECIO DE VENTA POR CADA KG DE HUEVO ***")
 
-# PROMPTING THE USER
+# PROMPTING THE USER AND INITIALIZING VARIABLES
 Gallinas = int(print("Cuántas gallinas hay en la granja: "))
 
+# INITIALIZING AND DECLARING VARIABLES
 i=0
 Promedio = 0
+Calidad = 0
 
+# RUNNING THE LOOP AND SAVING INFO
 while i < Gallinas:
   i += 1
 
+  # PROMTING THE USER
   Peso = float(print("Ingresar el peso de la gallina:"))
   Altura = float(print("Ingresar la altura de la gallina:"))
   Huevos = int(print("Ingresar el número de huevos que pone la gallina:"))
-  
-  Calidad = (Peso * Altura) / Huevos
+
+  # CALCULATING THE CHICKEN QUALITY  
+  Calidad = Calidad + (Peso * Altura) / Huevos
+
+Precio = Calidad / Gallinas
+
+print (f"El precio de venta por cada kg de huevo es: {Precio}.")
