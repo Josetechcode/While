c29b53ee1d91dec0db8d60aeecf18b9384cff134
Josetechcode <chema1987@hotmail.com>
2024-12-06 14:06:07 -0500

    while

--- eje9.py
+++ eje9.py
@@ -12,102 +12,80 @@ C = 0
 while i < Voters:
   i += 1
 
-  Choice = 0
-  while Choice != 1 and Choice != 2 and Choice != 3:
-    # PROMTING THE VOTER WHO THEY'RE VOTING FOR
-    Choice = int(input("Por cual candidato va a votar:\n1. Candidato A.\n2. Candidato B.\n3. Candidato C.\n"))
+  # PROMTING THE VOTER WHO THEY'RE VOTING FOR
+  Choice = int(input("Por cual candidato va a votar:\n1. Candidato A.\n2. Candidato B.\n3. Candidato C.\n"))
 
-    if Choice == 1:
-      A += 1 # ADDING UP VOTES TO CANDIDATE "A"
-      print (f"Se ha registrado el voto número {i}.")
-    elif Choice == 2:
-      B += 1 # ADDING UP VOTES TO CANDIDATE "B"
-      print (f"Se ha registrado el voto número {i}.")
-    elif Choice == 3:
-      C += 1 # ADDING UP VOTES TO CANDIDATE "C"
-      print (f"Se ha registrado el voto número {i}.")
+  if Choice == 1:
+    A += 1 # ADDING UP VOTES TO CANDIDATE "A"
+    print (f"Se ha registrado el voto número {i}.")
+  elif Choice == 2:
+    B += 1 # ADDING UP VOTES TO CANDIDATE "B"
+    print (f"Se ha registrado el voto número {i}.")
+  elif Choice == 3:
+    C += 1 # ADDING UP VOTES TO CANDIDATE "C"
+    print (f"Se ha registrado el voto número {i}.")
 
 Total = A + B + C # ADDING UP THE TOTAL OF THE VOTES
 
 if (A/Total)*100 >= 51:
-  print(f"El candidato A fue el ganador con {A} votos.\nEl candidato B obtuvo {B} votos.\nEl candidato C obtuvo {C} votos.")
+  print(f"El candidato A fue el ganador con {A} voto(s).\nEl candidato B obtuvo {B} voto(s).\nEl candidato C obtuvo {C} voto(s).")
 elif (B/Total)*100 >= 51:
-  print(f"El candidato B fue el ganador con {B} votos.\nEl candidato A obtuvo {A} votos.\nEl candidato C obtuvo {C} votos.")
+  print(f"El candidato B fue el ganador con {B} voto(s).\nEl candidato A obtuvo {A} voto(s).\nEl candidato C obtuvo {C} voto(s).")
 elif (C/Total)*100 >= 51:
-  print(f"El candidato C fue el ganador con {C} votos.\nEl candidato A obtuvo {A} votos.\nEl candidato B obtuvo {B} votos.")
+  print(f"El candidato C fue el ganador con {C} voto(s).\nEl candidato A obtuvo {A} voto(s).\nEl candidato B obtuvo {B} voto(s).")
 else:
-  print(f"Ningún candidato obtuvo el 51% de las votaciones.\nEl candidato A obtuvo {A} votos.\nEl candidato B obtuvo {B} votos.\nEl candidato C obtuvo {C} votos.")
+  print(f"Ningún candidato obtuvo el 51% de las votaciones.\nEl candidato A obtuvo {A} voto(s).\nEl candidato B obtuvo {B} voto(s).\nEl candidato C obtuvo {C} voto(s).")
+
+"""  print("la elección se repite en una segunda vuelta con los candidatos que quedaron en primero y segundo lugar.")
   
   if A > B and A > C:
     if B > C:
+      print("Los candidatos que van a segunda vuelta son A y B.")
       i = 0
       while i < Voters:
         i += 1
+        Choice = int(input("Por cual candidato va a votar:\n1. Candidato A.\n2. Candidato B.\n"))
 
-        Choice = 0
-        while Choice != 1 and Choice != 2:
-          # PROMTING THE VOTER WHO THEY'RE VOTING FOR
-          Choice = int(input("Por cual candidato va a votar:\n1. Candidato A.\n2. Candidato B.\n"))
-
-          if Choice == 1:
-            A += 1 # ADDING UP VOTES TO CANDIDATE "A"
-            print (f"Se ha registrado el voto número {i}.")
-          elif Choice == 2:
-            B += 1 # ADDING UP VOTES TO CANDIDATE "B"
-            print (f"Se ha registrado el voto número {i}.")
+        if Choice == 1:
+          A += 1 # ADDING UP VOTES TO CANDIDATE "A"
+          print (f"Se ha registrado el voto número {i}.")
+        elif Choice == 2:
+          B += 1 # ADDING UP VOTES TO CANDIDATE "B"
+          print (f"Se ha registrado el voto número {i}.")
           
-      if A > B:
-        print (f"El candidato A es el ganador con {A} votos")
-      elif B > A:
-        print (f"El candidato B es el ganador con {B} votos")
-      else:
-        print (f"El candidato A obtuvo {A} votos y el B obtuvo {B} votos. Dado que hay empate la elección se anula.")
-    
-    else:
+    elif C > B:
+      print("Los candidatos que van a segunda vuelta son A y C.")
       i = 0
       while i < Voters:
         i += 1
+        Choice = int(input("Por cual candidato va a votar:\n1. Candidato A.\n2. Candidato C.\n"))
 
-        Choice = 0
-        while Choice != 1 and Choice != 3:
-          # PROMTING THE VOTER WHO THEY'RE VOTING FOR
-          Choice = int(input("Por cual candidato va a votar:\n1. Candidato A.\n3. Candidato C.\n"))
+        if Choice == 1:
+          A += 1 # ADDING UP VOTES TO CANDIDATE "A"
+          print (f"Se ha registrado el voto número {i}.")
+        elif Choice == 2:
+          C += 1 # ADDING UP VOTES TO CANDIDATE "C"
+          print (f"Se ha registrado el voto número {i}.")
 
-          if Choice == 1:
-            A += 1 # ADDING UP VOTES TO CANDIDATE "A"
-            print (f"Se ha registrado el voto número {i}.")
-          elif Choice == 3:
-            C += 1 # ADDING UP VOTES TO CANDIDATE "B"
-            print (f"Se ha registrado el voto número {i}.")
-          
-      if A > C:
-        print (f"El candidato A es el ganador con {A} votos")
-      elif C > A:
-        print (f"El candidato C es el ganador con {C} votos")
-      else:
-        print (f"El candidato A obtuvo {A} votos y el C obtuvo {C} votos. Dado que hay empate la elección se anula.")
+  elif B > A and B > C:
+    if C > A:
+      print("Los candidatos que van a segunda vuelta son B y C.")
+      i = 0
+      while i < Voters:
+        i += 1
+        Choice = int(input("Por cual candidato va a votar:\n1. Candidato B.\n2. Candidato C.\n"))
 
+        if Choice == 1:
+          B += 1 # ADDING UP VOTES TO CANDIDATE "B"
+          print (f"Se ha registrado el voto número {i}.")
+        elif Choice == 2:
+          C += 1 # ADDING UP VOTES TO CANDIDATE "C"
+          print (f"Se ha registrado el voto número {i}.")
 
   else:
-    i = 0
-    while i < Voters:
-      i += 1
-
-      Choice = 0
-      while Choice != 1 and Choice != 3:
-        # PROMTING THE VOTER WHO THEY'RE VOTING FOR
-        Choice = int(input("Por cual candidato va a votar:\n2. Candidato B.\n3. Candidato C.\n"))
-
-        if Choice == 2:
-          B += 1 # ADDING UP VOTES TO CANDIDATE "A"
-          print (f"Se ha registrado el voto número {i}.")
-        elif Choice == 3:
-          C += 1 # ADDING UP VOTES TO CANDIDATE "B"
-          print (f"Se ha registrado el voto número {i}.")
-          
-      if B > C:
-        print (f"El candidato B es el ganador con {B} votos")
-      elif C > B:
-        print (f"El candidato C es el ganador con {C} votos")
-      else:
-        print (f"El candidato B obtuvo {B} votos y el C obtuvo {C} votos. Dado que hay empate la elección se anula.")
+    if A == B:
+      print("Por empate entre candidatos A y B la elección se declara nula.\nEl proceso de votación se debe realizar nuevamente desde cero.")
+    elif A == C:
+      print("Por empate entre candidatos A y C la elección se declara nula.\nEl proceso de votación se debe realizar nuevamente desde cero.")
+    elif B == C:
+      print("Por empate entre candidatos B y C la elección se declara nula.\nEl proceso de votación se debe realizar nuevamente desde cero.") """
