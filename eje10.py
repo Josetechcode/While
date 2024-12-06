37216ebbba99c47567015df0d2827ec9f48aaff8
Josetechcode <chema1987@hotmail.com>
2024-12-06 16:54:48 -0500

    while

--- eje10.py
+++ eje10.py
@@ -15,20 +15,23 @@ i = 0
 while i < Vehiculos:
   i += 1
   
-  Tipo = int(input("Ingresar el tipo de vehículo (1, 2,3, 4):\n1. Turismo.\n2. Autobus.\n3. Camión.\n4. Motoicleta.\n"))
+  Tipo = int(input("Ingresar el tipo de vehículo (1, 2, 3, 4):\n1. Turismo.\n2. Autobus.\n3. Camión.\n4. Motocicleta.\n"))
 
   if Tipo == 1:
-    Tur += Tur 
-    Passengers = input("Has ingresado exitosamente la entrada número {i} y quedan pendientes ",i-10,"entradas". (vehículo tipo turismo). Ingresar cantidad de ocupantes: ")
-    Passengers += Passengers
+    Tur = Tur + 1 
+    print(f"Has ingresado exitosamente la entrada número {i} y quedan pendientes ",Vehiculos-i,"entrada(s) (vehículo tipo turismo)")
+    Passengers1 = int(input("Ingresar cantidad de ocupantes: "))
+    Passengers += Passengers1 
   elif Tipo == 2:
-    Aut += Aut
-    print("Has ingresado exitosamente la entrada número {i} y quedan pendientes ",i-10,"entradas".  Vehículo tipo autobus.")
+    Aut = Aut + 1
+    print(f"Has ingresado exitosamente la entrada número {i} y quedan pendientes ",Vehiculos-i,"entrada(s) (Vehículo tipo autobus).")
   elif Tipo == 3:
-    Cam += Cam
-    print("Has ingresado exitosamente la entrada número {i} y quedan pendientes ",i-10,"entradas". Vehículo tipo camión.")
+    Cam = Cam + 1
+    print(f"Has ingresado exitosamente la entrada número {i} y quedan pendientes ",Vehiculos-i,"entrada(s) (Vehículo tipo camión).")
   elif Tipo == 4:
-    Mot += Mot 
-    print("Has ingresado exitosamente la entrada número {i} y quedan pendientes ",i-10,"entradas". Vehículo tipo motocicleta.")
+    Mot = Mot + 1 
+    print(f"Has ingresado exitosamente la entrada número {i} y quedan pendientes ",Vehiculos-i,"entrada(s) (Vehículo tipo motocicleta).")
 
-print(Tur, Cam, Aut, Mot,"Cantidad de turistas: ",Passengers)
+Total = Tur + Cam + Aut + Mot
+
+print("Cantidad de vehículos tipo turismo: ",Tur,"Correspondiete al",(Tur/Total)*100,"de la muestra\nCantidad de vehículos tipo camión: ",Cam,"Correspondiete al",(Cam/Total)*100,"de la muestra\nCantidad de vehículos tipo automovil: ",Aut,"Correspondiete al",(Aut/Total)*100,"de la muestra\nCantidad de vehículos tipo motocicleta: ",Mot,"Correspondiete al",(Mot/Total)*100,"de la muestra\nCantidad de turistas: ",Passengers,)
